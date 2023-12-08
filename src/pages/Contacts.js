0f370e51a5ec8fa9319ac40eaf575a6387d944fb
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-12-08 15:32:02 +0100

    Pages creation

---
+++ src/pages/Contacts.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { ContactList } from 'components/ContactList/ContactList';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectIsLoading } from 'redux/contacts/selectors';
+
+export default function Contacts() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Helmet>
+        <title>Your contacts</title>
+      </Helmet>
+      {/* <TaskEditor /> */}
+      <div>{isLoading && 'Request in progress...'}</div>
+      <ContactList />
+    </>
+  );
+}
