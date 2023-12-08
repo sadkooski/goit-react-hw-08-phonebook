47f6af185fccfd4b2231bdf9304781a486069350
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-12-08 01:49:24 +0100

    operations.js
    Create operation.js and all operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -25,3 +25,41 @@ export const fetchContacts = createAsyncThunk(
     }
   }
 );
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { contact });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const setFilter = createAsyncThunk(
+  'filter/setFilter',
+  async (filterValue, thunkAPI) => {
+    try {
+      const response = await axios.post(`/contacts`, {
+        filter: filterValue,
+      });
+      return response.data.filter;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,7 @@
 export const getContacts = state => state.contacts;
 
 export const getFilter = state => state.filter;
+
+export const getIsLoading = state => state.isLoading;
+
+export const getError = state => state.error;
