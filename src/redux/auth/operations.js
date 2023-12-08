e71303e378a613b5108f80100acb16c8b67c79d1
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-12-08 15:30:50 +0100

    redux auth

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { contact });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const setFilter = createAsyncThunk(
+  'filter/setFilter',
+  async (filterValue, thunkAPI) => {
+    try {
+      const response = await axios.post(`/contacts`, {
+        filter: filterValue,
+      });
+      return response.data.filter;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,7 @@
+export const selectContacts = state => state.contacts;
+
+export const selectFilter = state => state.filter;
+
+export const selectIsLoading = state => state.isLoading;
+
+export const selectError = state => state.error;
